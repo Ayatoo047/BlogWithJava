46f9fc20cf29457c2763a2f18d9ae53d8e349096
Ajibade Yakub Adebare <ajibadeyakubadebare@gmail.com>
2024-09-13 19:00:57 +0100

    Initial

---
+++ src/main/java/com/blogstart/blogstart/BlogstartApplication.java
@@ -0,0 +1,13 @@
+package com.blogstart.blogstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogstartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogstart/blogstart/BlogstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogstart.blogstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
